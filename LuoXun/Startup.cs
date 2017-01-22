324f897cb7840ce3a83855baa371eb6ccad3e40c
enjoysun <1570819807@qq.com>
2017-01-22 16:25:38 +0800

    demo第一版

---
+++ LuoXun/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LuoXun.Startup))]
+namespace LuoXun
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
